2937b2cb55a4b158e754d864920170c3901a30d1
Lang.Chen <snowf07@163.com>
2019-09-02 19:34:34 +0800

    --新增开机启动服务

--- progress-keep-alive/app/src/main/java/com/exmple/progress/keeplive/broadcast/MyBroadcastReceiver.java
+++ progress-keep-alive/app/src/main/java/com/exmple/progress/keeplive/broadcast/MyBroadcastReceiver.java
@@ -3,8 +3,10 @@ package com.exmple.progress.keeplive.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.exmple.progress.keeplive.service.LocalMountService;
+import com.exmple.progress.keeplive.service.MyJobService;
 
 /**
  * Created by lang.chen on 2019/9/2
@@ -19,6 +21,16 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
         } else if (intent.getAction().equals("android.intent.action.SCREEN_OFF")) {
             System.out.println("—— SCREEN_OFF ——");
+        }else if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
+            //开启广播
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                Intent intentA = new Intent(context, LocalMountService.class);
+                context.startService(intentA);
+            } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                //小于8.0 使用JobService
+                Intent intentB = new Intent(context, MyJobService.class);
+                context.startService(intentB);
+            }
         }
     }
 }
